1b5ce09cfb8c5aaa91a225a7b629a303e4ca2572
vivekmyers <vivek.myers@gmail.com>
2018-03-22 19:43:04 -0400

    Added image generation

---
+++ render.py
@@ -0,0 +1,30 @@
+import sys
+from numpy import zeros, uint8
+from scipy.misc import imsave
+from random import random
+
+grid = []
+
+for i in sys.stdin:
+    tmp = []
+    grid.append(tmp)
+    for j in i.split():
+        tmp.append(j)
+
+
+def new_color():
+    return [int(random() * 256) for _ in range(3)]
+
+
+colors = {}
+
+image = zeros([len(grid), len(grid[0]), 3], dtype=uint8)
+for x in range(len(grid)):
+
+    for y in range(len(grid[0])):
+        symbol = grid[x][y]
+        if symbol not in colors:
+            colors[symbol] = new_color()
+        image[x][y] = colors[symbol]
+
+imsave("output.png", image)
--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
 python3 ./solver.py > output.txt
+python3 ./render.py < output.txt
